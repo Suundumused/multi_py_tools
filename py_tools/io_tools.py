7c24faa4300aa25cdd70367f57a4ce1e450adf4f
Caio Silva <caiopacheco02@gmail.com>
2024-02-04 15:53:55 -0300

    init

---
+++ py_tools/io_tools.py
@@ -0,0 +1,19 @@
+def get_current_directory(args: tuple=()):
+    if getattr(sys, 'frozen', False):
+        path = os.path.dirname(sys.executable)#get if running as executable (PyInstaller) or any for end user...
+    else:
+        path = os.path.dirname(os.path.abspath(sys.argv[0]))#else if Running as .py script.
+    
+    if args:
+        return os.path.join(path, ''.join(args))
+    else:
+        return path
+    
+    
+if __name__ == "__main__":
+    import os
+    import sys
+        
+    #test any func...
+    
+    print(get_current_directory(('some_folder', 'text.txt')))
