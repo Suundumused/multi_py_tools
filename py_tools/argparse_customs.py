f4974d06cb75bd7262b6e8ae1b4ba1db90e94635
Caio Silva <caiopacheco02@gmail.com>
2024-02-05 17:29:18 -0300

    Create argparse_customs.py

---
+++ py_tools/argparse_customs.py
@@ -0,0 +1,32 @@
+def str_bool_switcher_type(arg):
+    try:
+        if arg.lower() == 'true' or arg.lower() == 'false':
+            return eval(arg)
+        else:
+            return arg
+              
+    except ValueError:
+        raise argparse.ArgumentTypeError("Invalid type. Must be str/bool")
+
+
+def tuple_type(arg):
+    try:
+        values = [int(item) if item.isdigit() else item for item in arg.split(',')]
+        return tuple(values)
+    
+    except ValueError:
+        raise argparse.ArgumentTypeError("Invalid tuple format. Must be comma-separated integers/strings without spaces.")
+
+
+if __name__ == "__main__":
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-strbool', '--strboolswitcher', type=str_bool_switcher_type, default=False)
+    parser.add_argument('-tuples', '--tuple_options', type=tuple_type, default=('hey', 33))
+    
+    args = parser.parse_args() 
+    
+    print(args.strboolswitcher)
+    print(args.tuple_options)
